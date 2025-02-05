d210b3a1db81f2f5e704317a6f9e57d2c95831ab
giovannipanico <giovanni.panico@lutech.it>
2025-02-05 18:33:53 +0100

    Fix build script for Netlify

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,27 @@ var sass = require('gulp-sass')(require('sass'));
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-// compile scss to css
+// Compile SCSS to CSS
 gulp.task('sass', function () {
     return gulp.src('./sass/styles.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({basename: 'styles.min'}))
-        .pipe(gulp.dest('./css'));
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ basename: 'styles.min' }))
+        .pipe(gulp.dest('./dist/css')); // Salva i file CSS in dist/css
 });
 
-// watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-// minify js
+// Minify JS
 gulp.task('minify-js', function () {
     return gulp.src('./js/scripts.js')
         .pipe(uglify())
-        .pipe(rename({basename: 'scripts.min'}))
-        .pipe(gulp.dest('./js'));
+        .pipe(rename({ basename: 'scripts.min' }))
+        .pipe(gulp.dest('./dist/js')); // Salva i file JS in dist/js
 });
 
-// default task
-gulp.task('default', gulp.series('sass', 'minify-js'));
+// Copy HTML files to dist
+gulp.task('copy-html', function () {
+    return gulp.src('./*.html')
+        .pipe(gulp.dest('./dist')); // Copia i file HTML in dist
+});
+
+// Default task
+gulp.task('default', gulp.series('sass', 'minify-js', 'copy-html'));
